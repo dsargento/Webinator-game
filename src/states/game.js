d1664c2cb404b7cde0638d252789604083a57a27
Archaons92i <pakel_l@etna-alternance.net>
2017-01-05 10:55:07 +0100

    game

--- src/states/game.js
+++ src/states/game.js
@@ -9,38 +9,115 @@ Game.prototype.create = function() {
     xpos = this.world.centerX;
     ypos = this.world.centerY;
     this.input.onDown.add(this.onInputDown, this);
-    this.createP1();
-    this.createP2();
-
-    // just a property we can tween so the bar has a progress to show
-    barProgress = 128;
-
-    // the bar itself
-    bar = this.add.bitmapData(128, 8);
-
-    this.add.sprite(this.world.centerX - (bar.width * 0.5), this.world.centerY, bar);
-
+    if (this.createP1() == true)
+    {
+      player1Alive = true;
+      p1attack = 6;
+      p1init = 10;
+      p1avoid = 50;
+      p1armor = 3;
+      p1life = 100;
+      p1block = 10;
+    }
+    if (this.createP2() == true)
+    {
+      p1init = 20;
+      player2Alive = true;
+      p2attack = 10;
+      p2avoid = 20;
+      p2armor = 4;
+      p2life = 100;
+      p2block = 10;
+    }
+    this.CheckPlayersInit();
+    gameTurn = 0;
 
 };
 
 Game.prototype.update = function() {
-    bar.context.clearRect(0, 0, bar.width, bar.height);
-
-    // some simple colour changing to make it look like a health bar
-    if (barProgress < 32) {
-        bar.context.fillStyle = '#f00';
-    } else if (barProgress < 64) {
-        bar.context.fillStyle = '#ff0';
-    } else {
-        bar.context.fillStyle = '#0f0';
+  if((this.player1Alive == true) && (this.player2Alive == true))
+  {
+    if(this.gameTurn == 0)
+    {
+      this.Player1Turn();
+      this.gameTurn = 1;
     }
-
-    // draw the bar
-    bar.context.fillRect(0, 0, barProgress, 8);
-
-    // important - without this line, the context will never be updated on the GPU when using webGL
-    bar.dirty = true;
+    else if(this.gameTurn == 1)
+    {
+      this.Player2Turn();
+      this.gameTurn = 0;
+    }
+    CheckPlayersAreAlive();
+  }
 };
+
+Game.prototype.Player1Turn = function() {
+  //CheckWeapon();
+  Attack(this.p2life, this.p2armor, this.p2avoid, this.p1attack);
+};
+
+
+Game.prototype.Player2Turn = function() {
+  //CheckWeapon();
+  Attack(this.p1life,this. p1armor, this.p1avoid, this.p2attack);
+};
+
+Game.prototype.CheckPlayersAreAlive = function() {
+  if(this.p1life <= 0)
+  {
+    this.player1Alive = false;
+  }
+  else   if(this.p2life <= 0) {
+    this.player2Alive = false;
+  }
+};
+
+
+Game.prototype.Attack = function(life, armor, avoid, attack) {
+
+  if(life > 0)
+  {
+    damage = attack - armor;
+    var rand1 = this.rnd.integerInRange(0, 100);
+    if (avoid < rand1)
+    {
+      life = life - damage;
+    }
+    else {
+      {
+        console.log('avoid');
+      }
+    }
+  }
+};
+/*Game.prototype.CheckWeapon = function() {
+
+};*/
+
+Game.prototype.CheckPlayersInit = function() {
+  if(this.p1init > this.p2init)
+  {
+    this.gameTurn = 0;
+  }
+  else if (this.p1init  < this.p2init)
+  {
+    this.gameTurn = 1;
+  }
+  else if (this.p1init  ==  this.p2init)
+  {
+    var rand1 = this.rnd.integerInRange(0, 100);
+    var rand2 = this.rnd.integerInRange(0, 100);
+    if(rand1 < rand2)
+    {
+      this.gameTurn = 0;
+    }
+    else
+    {
+      this.gameTurn = 1;
+    }
+  }
+};
+
 
 Game.prototype.onInputDown = function() {
     // this.game.state.start('gameover');
